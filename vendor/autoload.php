9889fe585c29331a047cdf6ccdeddbde1b43b668
Ayus Irfang Filaras <ayus.sahabat@gmail.com>
2017-03-09 00:39:11 +0700

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -1,1 +1,25 @@
-#!/vendor/
+#!/vendor;
+<?php
+/**
+ * Apolbox - Autoload
+ *
+ * @package apolbox
+ * @since version 1.0
+ *
+ * @author Ayus Irfang Filaras <ayus.sahabat@gmail.com>
+ */
+@error_reporting(0);
+
+// Menonaktifkan fungsi spl_autoload() standard.
+@spl_autoload_register();
+
+// Menonaktifkan __autoload() jika aktif.
+if ( function_exists('__autoload') )
+  @__autoload();
+
+// Membuat kembali fungsi autoload
+spl_autoload_register(function ( $class ) {
+  
+  // Cek __NAMESPACE__
+  
+});
