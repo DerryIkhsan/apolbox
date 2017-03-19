92cc705f93e5c95da951e0c901bf1c7b1753f897
Ayus Irfang Filaras <ayus.sahabat@gmail.com>
2017-03-19 22:12:50 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,5 +17,43 @@
  * @lisence https://apolbox.com/license.txt
  */
 function buildProject() {
+  
+  // cek build.properties
+  $buildProperties = getProjectDirectory() . '/build.properties';
+  
+  // File build.properties adalah file berformat json
+  // kita akan mengkonvert menjadi sebuah array+.
+  // Membuka file build.properties
+  if ( $openBuildProperties = fopen( $buildProperties, 'r+ ) ) {
+    // Setelah terbuka sekarang saatnya membaca isi file.
+    $readBuildProperties = fread( $openBuildProperties, 4096 );
+    
+    // Konvert isi build.properties menjadi sebuah array
+    // menggunakan fungsi json_encode dan json_decode
+    $konvertToArray = json_encode( $readBuildProperties  );
+    
+    // Setelah menjadi file array saatnya membongkar isi dari array.
+    // dan mencoba untuk memuat isi dari array.
+    extractArrayToString( $konvertToArray, 'loadArrayIsFile' );
+  }
+}
 
+function getProjectDirectory() {
+  return dirname(__DIR__);
+}
+
+function extractArrayToString( $array, $commands ) {
+  foreach( $array as $string ) {
+    switch( $commands ) {
+      case 'loadArrayIsFile':
+        loadArrayIsFile( $string );
+        break;
+    }
+  }
+}
+
+function loadArrayIsFile( $array ) {
+  if ( is_file( $array ) ) {
+    require getProjectDirectory() . $array;
+  }
 }
