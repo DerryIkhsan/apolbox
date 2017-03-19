c060e13434a13471174e38870fbbe9cf770b701b
Ayus Irfang Filaras <ayus.sahabat@gmail.com>
2017-03-20 00:38:56 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -38,16 +38,83 @@ function buildProject() {
   }
 }
 
+// Setup project directory
 function getProjectDirectory() {
   return dirname(__DIR__);
 }
 
+// Extrack array menjadi string
 function extractArrayToString( $array, $commands ) {
-    print_r( $array );
+    try {
+        // Jika $array bukan sebuah array dan $array tidak dalam array
+        // maka laporkan error compiler file gagal.
+        if ( ! is_array( $array ) && ! in_array( $array ) ) {
+            throw new Exception("Compiler file gagal.", 1);
+        }
+
+        // mencari kata kunci repositories di dalam array
+        if ( array_key_exists( 'repositories' , $array ) ) {
+            // Cek fungsi repositories sudah ada.
+            // jika tidak ada laporkan error compiler repositories failed.
+            if ( ! function_exists( 'repositories' ) ) {
+                throw new Exception("Compiler repositories failed.", 1);
+            }
+            repositories( $array['repositories'] );
+        }
+
+        // Mencari kata kunci allProject di dalam array
+        if ( array_key_exists( 'allProject', $array ) ) {
+            // Cek fungsi allProject sudah ada.
+            // jika tidak ada laporkan error compiler project failed.
+            if ( ! function_exists( 'allProject') ) {
+                throw new Exception("Compiler project failed", 1);
+            }
+            allProject( $array['allProject'] );
+        }
+
+    } catch (Exception $e) {
+        printf( "Error on line %s", $e->getLine() );
+        printf( "\rin %s", $e->getFIle() );
+        printf( "\r: %s", $e->getMessage() );
+
+        return false;
+    }
+
 }
 
-function loadArrayIsFile( $array ) {
+function loadArrayIsFile( $arrayFiles ) {
     require getProjectDirectory() . $array;
+}
+
+function repositories( $repo ) {
+    // extract repo and checking
+    foreach ($repo as $key => $value) {
+        try {
+            // checking function on key
+            if ( ! function_exists( $key ) ) {
+                throw new Exception("Fungsi $key tidak dapat dimuat. <br/>", 1);
+            }
+            $key( $repo[$key] );
+        } catch (Exception $e) {
+            printf( "Error on line %s", $e->getLine() );
+            printf( "\rin %s", $e->getFIle() );
+            printf( "\r: %s", $e->getMessage() );
+
+            return false;
+        }
+    }
+}
+
+function allProject( $project ) {
+    //print_r( $project );
+}
+
+function package( $packagist ) {
+    var_dump( $packagist );
+}
+
+function vendor( $vendor ) {
+    var_dump( $vendor );
 }
 
 return buildProject();
