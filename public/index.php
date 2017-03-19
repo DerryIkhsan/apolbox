172d994c55b0900870c84803c4973dee3f6a8130
Ayus Irfang Filaras <ayus.sahabat@gmail.com>
2017-03-19 21:54:16 +0700

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Apolbox - Framework Productiont
+ *
+ * Apolbox adalah situs kerangka kerja untuk membangun situs web,
+ * dan juga sebagai aplikasi jejaring untuk sarana informasi, pertemanan dan cinta.
+ *
+ * @package apolbox
+ * @subpackage administrator
+ *
+ * @copyright (c) [29 Juni 2016]
+ * @since version 1.0
+ *
+ * @author Ayus Irfang Filaras <ayus.sahabat@gmail.com>
+ * @url git clone https://github.com/apolbox-project/apolbox.git
+ *
+ * @lisence https://apolbox.com/license.txt
+ */
+function buildProject() {
+
+}
