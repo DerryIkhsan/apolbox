9c6447231249f80f3fd6d0333fcaac9b78ad888f
apolbox-project <ayus.sahabat@gmail.com>
2017-03-20 16:30:17 +0700

    update index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,7 @@
 /**
  * Apolbox - Framework Productiont
  *
- * Apolbox adalah situs kerangka kerja untuk membangun situs web,
- * dan juga sebagai aplikasi jejaring untuk sarana informasi, pertemanan dan cinta.
+ * Apolbox adalah situs kerangka kerja untuk membangun situs web.
  *
  * @package apolbox
  * @subpackage administrator
