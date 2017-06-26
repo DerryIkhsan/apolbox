eddc67186e1d67b49fbdc5508ba2369083edabb9
apolbox <ayus.sahabat@gmail.com>
2017-06-26 15:01:42 +0700

    update util

--- util.php
+++ util.php
@@ -8,8 +8,20 @@
  *
  * (c) Ayus irfang filaras
  */
-spl_autoload_register(function($classname){
-	$filename = str_replace('\\', '/', $classname).'.php';
-	if (!file_exists($filename)) @exit("Tidak dapat menemukan file atau direktori.");
-	@include $filename;
-});
+spl_autoload_register('loadClass');
+
+/**
+ * Fungsi untuk memuat kelas
+ * @var string/object $classname
+ */
+function loadClass($classname) {
+	$classname = str_replace(
+		"\\",
+		"/",
+		"{$classname}.php"
+	);
+
+	if (!file_exists($classname)) @exit("Tidak dapat menemukan file atau direktori.");
+	else if (is_dir($classname)) @exit("Ini adalah direktori");
+	else @include $classname;
+}
